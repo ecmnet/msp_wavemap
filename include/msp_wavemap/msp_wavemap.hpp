9583337368ad35a3c5b9e98f78007f869f1d28b4
ecmnet <ecm@gmx.de>
2025-02-12 13:26:21 +0100

    Transformer helper moved to msp_controller

--- include/msp_wavemap/msp_wavemap.hpp
+++ include/msp_wavemap/msp_wavemap.hpp
@@ -4,6 +4,7 @@
 #include <msp_msgs/srv/trajectory_check.hpp>
 #include <px4_msgs/msg/vehicle_attitude.hpp>
 #include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <px4_msgs/msg/vehicle_status.hpp>
 #include <px4_msgs/msg/obstacle_distance.hpp>
 // #include <msp_msgs/msg/trajectory.hpp>
 #include <msp_wavemap/ros2/ros2map_publisher.hpp>
@@ -14,7 +15,7 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-#include "msp_wavemap/ros2/tf_transformer.h"
+#include "msp_controller/ros2/tf_transformer.h"
 
 #include <wavemap/core/config/config_base.h>
 #include <wavemap/core/indexing/index_hashes.h>
@@ -23,7 +24,6 @@
 #include <wavemap/core/utils/thread_pool.h>
 #include <wavemap/pipeline/pipeline.h>
 
-#include <sensor_msgs/msg/laser_scan.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 
@@ -35,14 +35,14 @@ namespace msp
   class MSWaveMapNode : public msp::MSPNodeBase // public rclcpp::Node
   {
   public:
-    explicit MSWaveMapNode() : msp::MSPNodeBase("MSWaveMapNode", MSP_COMP_AVOID), transformer_(std::make_shared<TfTransformer>())
+    explicit MSWaveMapNode() : msp::MSPNodeBase("MSWaveMapNode", MSP_COMP_AVOID)
     {
 
       gz_node = std::make_unique<gz::transport::Node>();
 
-      msp_trajectory_check = this->create_service<msp_msgs::srv::TrajectoryCheck>(
-          "/msp/in/trajectory_check",
-          std::bind(&MSWaveMapNode::onTrajectoryCheck, this, std::placeholders::_1, std::placeholders::_2));
+      // msp_trajectory_check = this->create_service<msp_msgs::srv::TrajectoryCheck>(
+      //     "/msp/in/trajectory_check",
+      //     std::bind(&MSWaveMapNode::onTrajectoryCheck, this, std::placeholders::_1, std::placeholders::_2));
 
       initialize();
     }
@@ -61,8 +61,6 @@ namespace msp
     // Map management pipeline
     std::shared_ptr<Pipeline> pipeline_;
 
-    std::shared_ptr<TfTransformer> transformer_;
-
     Eigen::Vector3f pos_ = Eigen::Vector3f(0.0f, 0.0f, 0.0f);
     cv::Mat image;
 
@@ -73,7 +71,6 @@ namespace msp
 
     rclcpp::Service<msp_msgs::srv::TrajectoryCheck>::SharedPtr msp_trajectory_check;
 
-    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_publisher_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_broadcaster_;
 
@@ -84,35 +81,31 @@ namespace msp
     void onTrajectoryCheck(const std::shared_ptr<msp_msgs::srv::TrajectoryCheck::Request> request,
                            std::shared_ptr<msp_msgs::srv::TrajectoryCheck::Response> response);
 
-    bool checkCollision(Eigen::Vector3f position, const float radius, const uint8_t points);
-
-    void publishScan(std::vector<float> scans, Eigen::Vector3f position);
-
-    void publish_camera_transform() {
-
+    void publish_camera_transform()
+    {
 
       static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
       geometry_msgs::msg::TransformStamped transformStamped;
 
       transformStamped.header.stamp = this->get_clock()->now();
-      transformStamped.header.frame_id = "base_link";   // Parent frame
-      transformStamped.child_frame_id = "camera_link";  // Child frame
+      transformStamped.header.frame_id = "base_link";  // Parent frame
+      transformStamped.child_frame_id = "camera_link"; // Child frame
 
       // Translation (Camera position relative to body frame)
-      transformStamped.transform.translation.x = 0.1;  // 10 cm forward
-      transformStamped.transform.translation.y = 0.0;  // 5 cm right
-      transformStamped.transform.translation.z = 0.0;  // 20 cm up
+      transformStamped.transform.translation.x = 0.1; // 10 cm forward
+      transformStamped.transform.translation.y = 0.0; // 5 cm right
+      transformStamped.transform.translation.z = 0.0; // 20 cm up
 
       tf2::Quaternion q;
-      q.setRPY(-M_PI_2, 0,-M_PI_2); 
+      q.setRPY(-M_PI_2, 0, -M_PI_2);
       transformStamped.transform.rotation.x = q.x();
       transformStamped.transform.rotation.y = q.y();
       transformStamped.transform.rotation.z = q.z();
       transformStamped.transform.rotation.w = q.w();
 
       static_broadcaster_->sendTransform(transformStamped);
-  }
+    }
   };
 
 } // namespace msp
--- src/msp_wavemap.cpp
+++ src/msp_wavemap.cpp
@@ -12,13 +12,13 @@
 #include <wavemap/core/map/map_factory.h>
 #include <wavemap/pipeline/map_operations/map_operation_factory.h>
 #include <msp_wavemap/lib/map_operations/msp_map_operation_factory.h>
-#include <wavemap/core/utils/query/map_interpolator.h>
+
+#include <wavemap/core/utils/math/approximate_trigonometry.h>
 
 #include <opencv2/core/eigen.hpp>
 #include <wavemap/pipeline/pipeline.h>
 #include <wavemap/core/utils/query/map_interpolator.h>
 #include <wavemap/core/utils/iterate/grid_iterator.h>
-#include <wavemap/core/utils/query/query_accelerator.h>
 #include <msp_msgs/msg/trajectory.hpp>
 
 #include <msp_wavemap/lib/config/stream_conversions.h>
@@ -40,7 +40,6 @@ void MSWaveMapNode::initialize()
 
   // trajectory_publisher_ = this->create_publisher<msp_msgs::msg::Trajectory>("msp/in/trajectory", this->getQos());
   obstacle_publisher_ = this->create_publisher<px4_msgs::msg::ObstacleDistance>("msp/in/obstacle_distance", this->getQos());
-  scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>("/scan", 10);
   tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
   std::string base_path = ament_index_cpp::get_package_share_directory("msp_wavemap");
@@ -85,7 +84,6 @@ void MSWaveMapNode::initialize()
 
     const auto type_name = param::getTypeStr(operation_params);
 
-
     if (const auto type = MapMSPOperationType{type_name.value()};
         type.isValid())
     {
@@ -114,8 +112,8 @@ void MSWaveMapNode::initialize()
 
   map_publisher.setMap(occupancy_map_);
 
-   // Publish Camera Tranform
-   publish_camera_transform();
+  // Publish Camera Tranform
+  publish_camera_transform();
 
   RCLCPP_INFO(this->get_logger(), "MSP WaveMap Node started");
 }
@@ -128,7 +126,7 @@ void MSWaveMapNode::onDepthReceived(const gz::msgs::Image &msg)
   const auto T_W_C =
       transformer_->lookupLatestTransform("world", "camera_link");
 
-  if(T_W_C == std::nullopt)
+  if (T_W_C == std::nullopt)
     return;
 
   pos_ = T_W_C->getPosition();
@@ -144,11 +142,8 @@ void MSWaveMapNode::onDepthReceived(const gz::msgs::Image &msg)
 
   pipeline_->runPipeline({"gazebo_short", "gazebo_long"}, depth_image);
   auto t2 = std::chrono::high_resolution_clock::now();
- // checkCollision(T_W_C->getPosition(), 7.0f, 72);
 
-  auto t3 = std::chrono::high_resolution_clock::now();
-  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1000 << "us\n";
-  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t3 - t2).count() / 1000 << "us\n";
+  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1000000 << "ms\n";
   const size_t map_size_KB = occupancy_map_->getMemoryUsage() / 1024;
   std::cout << "Created map of size: " << map_size_KB << " KB" << std::endl;
 }
@@ -157,128 +152,6 @@ void MSWaveMapNode::onTrajectoryCheck(const std::shared_ptr<msp_msgs::srv::Traje
                                       std::shared_ptr<msp_msgs::srv::TrajectoryCheck::Response> response)
 {
   response->reply.status = msp_msgs::msg::TrajectoryCheckAck::STATUS_NO_COLLISION;
-}
-
-bool MSWaveMapNode::checkCollision(Eigen::Vector3f position, const float radius, const uint8_t points)
-{
-
-  // if (pos_.z() < 1.0f)
-  //   return false;
-
-  const float min_cell_width = occupancy_map_->getMinCellWidth();
-  const Point3D radius_vec(radius, radius, min_cell_width);
-
-  const Point3D lower_bound(position - radius_vec);
-  const Point3D upper_bound(position + radius_vec);
-
-  Point3D lower_bound_b;
-
-  const auto T_W_C =
-      transformer_->lookupLatestTransform("world", "camera_link");
-  
-
-  const float inv_min_cell_width = 1.0f / min_cell_width;
-
-  const Index3D min_corner_index = convert::pointToFloorIndex(lower_bound, inv_min_cell_width);
-  const Index3D max_corner_index = convert::pointToCeilIndex(upper_bound, inv_min_cell_width);
-
-  std::vector<Point3D> occupied_cells;
-
-  const auto *hashed_map = dynamic_cast<const HashedChunkedWaveletOctree *>(occupancy_map_.get());
-  CHECK_NOTNULL(hashed_map);
-
-  QueryAccelerator<HashedChunkedWaveletOctree> query_accelerator(*hashed_map);
-
-  for (const auto &query_index : Grid<3>(min_corner_index, max_corner_index))
-  {
-
-    auto value = query_accelerator.getCellValue(query_index);
-    const float probability = convert::logOddsToProbability(value);
-    if (probability > 0.7f)
-    {
-      auto cell_location = convert::indexToCenterPoint(query_index, min_cell_width);
-      if ((cell_location - position).norm() < radius)
-      {
-        occupied_cells.push_back(cell_location);
-      }
-    }
-  }
-
-  // if (occupied_cells.size() == 0)
-  // {
-  //   in_collision = false;
-  //   return false;
-  // }
-
-  const float PIP = points / M_PI;
-
-  std::vector<float> distance_bins(points, std::numeric_limits<float>::max());
-
-  const float ref_angle = std::atan2(position.y(), position.x());
-
-  for (auto point : occupied_cells)
-  {
-
-    float delta_x = point.x() - position.x();
-    float delta_y = point.y() - position.y();
-    float xy_angle = std::atan2(delta_y, delta_x) - ref_angle;
-
-    if ((xy_angle < -M_PI_2) || xy_angle > M_PI_2)
-      continue;
-
-    float distance = (point - position).norm();
-    const int index = static_cast<int>((xy_angle)*PIP);
-    if (index < 0 || index > 71)
-      continue;
-
-    if (distance < distance_bins[index])
-      distance_bins[index] = distance;
-  }
-
-  publishScan(distance_bins, position);
-
-  // if (!in_collision && occupied_cells.size() > 1)
-  // {
-  //   this->log_message("[msp] Emergency breaking.", MAV_SEVERITY_ALERT);
-  //   in_collision = true;
-  //   return true;
-  // }
-
-  return false;
-}
-
-void MSWaveMapNode::publishScan(std::vector<float> scans, Eigen::Vector3f position)
-{
-
-  const float PI2 = M_PI / 2;
-
-  auto scan_msg = sensor_msgs::msg::LaserScan();
-  scan_msg.header.stamp = this->get_clock()->now();
-  scan_msg.header.frame_id = "base_line"; // Set the correct TF frame
-
-  // Laser properties
-  scan_msg.angle_min = -M_PI_2;
-  scan_msg.angle_max =  M_PI_2;
-  scan_msg.angle_increment = M_PI / 72;
-  scan_msg.time_increment = 0.0;
-  scan_msg.scan_time = 0.1;
-  scan_msg.range_min = 0.1;
-  scan_msg.range_max = 7.0;
-
-  scan_msg.ranges.resize(72, 0.0);
-  scan_msg.intensities.resize(72, 0.0);
-
-  // Generate fake distance readings
-  for (int i = 0; i < 72; i++)
-  {
-    if (scans[i] > 7.0)
-      continue;
-    scan_msg.intensities[i] = 1.0f;
-    scan_msg.ranges[i] = scans[i];
-  }
-
-  // Publish the scan message
-  scan_publisher_->publish(scan_msg);
 }
 
 int main(int argc, char *argv[])
