88eba32fc9c0ffbc7c9b59e12bc1157214eba40e
ecmnet <ecm@gmx.de>
2025-02-25 07:11:28 +0100

    Grid publishing accelerated

--- include/msp_wavemap/ros2/ros2_grid_publisher.hpp
+++ include/msp_wavemap/ros2/ros2_grid_publisher.hpp
@@ -6,7 +6,6 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-
 #include <wavemap/core/map/map_base.h>
 #include <wavemap/core/map/hashed_chunked_wavelet_octree.h>
 #include <wavemap/core/map/hashed_wavelet_octree.h>
@@ -27,22 +26,24 @@ using namespace wavemap;
 namespace msp
 {
     class MSPRos2GridPublisher
+       
     {
+        using BlockIndex = Index3D;
+        using CellIndex = OctreeIndex;
 
     public:
         explicit MSPRos2GridPublisher(rclcpp::Node *node, std::shared_ptr<TfTransformer> transformer) : _node(node), transformer_(transformer)
         {
 
-            microgrid_publisher_ = _node->create_publisher<msp_msgs::msg::MicroGrid>("msp/in/micro_grid", 100);
-
-            timer_ = _node->create_wall_timer(
-                std::chrono::milliseconds(50),
-                std::bind(&MSPRos2GridPublisher::publish, this));
+            microgrid_publisher_ = _node->create_publisher<msp_msgs::msg::MicroGrid>("msp/in/micro_grid", 1);
         }
 
         void setMap(wavemap::MapBase::Ptr map)
         {
             _map = map;
+            timer_ = _node->create_wall_timer(
+                std::chrono::milliseconds(50),
+                std::bind(&MSPRos2GridPublisher::publish, this));
         }
 
     private:
@@ -56,12 +57,12 @@ namespace msp
             publish_grid(*T_W_C);
         }
 
-        void transfer_grid(std::vector<Point3D> *list, Point3D r)
+        void transfer_grid(std::vector<uint64_t> *list, Point3D r)
         {
-             msp_msgs::msg::MicroGrid grid;
+            msp_msgs::msg::MicroGrid grid;
 
-            if(list->size() == 0)
-              return;
+            if (list->size() == 0)
+                return;
 
             int count = 0;
 
@@ -70,17 +71,16 @@ namespace msp
             grid.center[2] = r.z();
             grid.resolution = 0.2f;
             grid.extension = 10.0f;
-            
-            
+
             grid.count = 0;
             while (list->size() > 0)
             {
-                auto p = list->back();
+                grid.data[grid.count] = list->back();
                 list->pop_back();
-                grid.data[grid.count] = encode(p, grid.extension, grid.resolution);
-                if (grid.count >= 24)
-                {   count++;
-                   microgrid_publisher_->publish(grid);
+                if (grid.count >= 25)
+                {
+                    count++;
+                    microgrid_publisher_->publish(grid);
                     grid.count = 0;
                     continue;
                 }
@@ -92,7 +92,7 @@ namespace msp
 
         void publish_grid(Transformation3D twc)
         {
-            std::vector<Point3D> list;
+            std::vector<uint64_t> add, remove;
 
             const Eigen::Vector3d reference(twc.getPosition().x(), twc.getPosition().y(), twc.getPosition().z());
 
@@ -120,19 +120,26 @@ namespace msp
                 for (const auto &query_index :
                      wavemap::Grid<3>(min_corner_index, max_corner_index))
                 {
+                   
+                    const BlockIndex block_index = convert::indexToBlockIndex(query_index, hashed_wavelet_octree->getTreeHeight());
+                    const auto *b = hashed_wavelet_octree->getBlock(block_index);
+                    if (!b || b->getTimeSinceLastUpdated() > 0.1f)
+                        continue;
+             
                     const wavemap::FloatingPoint occupancy_log_odds = query_accelerator.getCellValue(query_index);
-                    if (occupancy_log_odds > 0.5)
-                    {
-                        const auto index = OctreeIndex{0, query_index};
-                        const Point3D block = convert::indexToCenterPoint(index.position, min_cell_width);
 
-                        list.emplace_back(block);
-                    }
+                    const auto index = OctreeIndex{0, query_index};
+                    const Point3D block = convert::indexToCenterPoint(index.position, min_cell_width);
+                    if (occupancy_log_odds > 0.5)
+                        add.emplace_back(encode(block, radius_vec.x(), min_cell_width, 0.7f));
+                    if (occupancy_log_odds < -1.0)
+                        remove.emplace_back(encode(block, radius_vec.x(), min_cell_width, 0));
+                   
                 }
             }
 
-            if (const auto *hashed_chunked_wavelet_octree =
-                    dynamic_cast<const HashedWaveletOctree *>(_map.get());
+            if ( const auto *hashed_chunked_wavelet_octree =
+                    dynamic_cast< const HashedChunkedWaveletOctree *>(_map.get());
                 hashed_chunked_wavelet_octree)
             {
 
@@ -143,31 +150,41 @@ namespace msp
                 const wavemap::Index3D max_corner_index =
                     wavemap::convert::pointToCeilIndex(upper_bound_wavemap,
                                                        1.f / min_cell_width);
-
+                    
                 wavemap::QueryAccelerator query_accelerator(*hashed_chunked_wavelet_octree);
                 for (const auto &query_index :
                      wavemap::Grid<3>(min_corner_index, max_corner_index))
                 {
-                    const wavemap::FloatingPoint occupancy_log_odds = query_accelerator.getCellValue(query_index);
-                    if (occupancy_log_odds > 0.5)
-                    {
-                        const auto index = OctreeIndex{0, query_index};
-                        const Point3D block = convert::indexToCenterPoint(index.position, min_cell_width);
 
-                        list.emplace_back(block);
-                    }
+                    const BlockIndex block_index = convert::indexToBlockIndex(query_index, hashed_chunked_wavelet_octree->getTreeHeight());
+                    const auto *b = hashed_chunked_wavelet_octree->getBlock(block_index);
+                    if (!b || b->getTimeSinceLastUpdated() > 0.1f)
+                        continue;
+                   
+                    const wavemap::FloatingPoint occupancy_log_odds = query_accelerator.getCellValue(query_index);
+                    
+                    const auto index = OctreeIndex{0, query_index};
+                    const Point3D block = convert::indexToCenterPoint(index.position, min_cell_width);
+
+                    if (occupancy_log_odds > 0.5)
+                        add.emplace_back(encode(block, radius_vec.x(), min_cell_width, 0.7f));
+                    if (occupancy_log_odds < -1.0)
+                        remove.emplace_back(encode(block, radius_vec.x(), min_cell_width, 0));
+
                 }
             }
- 
-            transfer_grid(&list, reference.cast<float>()); 
+
+            transfer_grid(&remove, reference.cast<float>());
+            transfer_grid(&add, reference.cast<float>());
         }
 
-        uint64_t encode(Point3D p, float extension, float resolution)
+        uint64_t encode(Point3D p, float extension, float resolution, float value = 0)
         {
-            uint16_t f = static_cast<uint16_t>( extension / resolution );
-            return (static_cast<uint64_t>( (p.x() + f ) * f * 2) & 0x1FFFFFUL ) |
-                   (static_cast<uint64_t>( (p.y() + f ) * f * 2) & 0x1FFFFFUL ) << 21 |
-                   (static_cast<uint64_t>( (p.z() + f ) * f * 2) & 0x1FFFFFUL ) << 42;
+            uint16_t f = static_cast<uint16_t>(extension / resolution);
+            return (static_cast<uint64_t>((value) * 4096.0f) & 0xFFFUL) |
+                   (static_cast<uint64_t>((p.x() + f) * f * 2) & 0x1FFFFUL) << 12 |
+                   (static_cast<uint64_t>((p.y() + f) * f * 2) & 0x1FFFFUL) << 29 |
+                   (static_cast<uint64_t>((p.z() + f) * f * 2) & 0x1FFFFUL) << 46;
         }
 
         rclcpp::Node *_node;
